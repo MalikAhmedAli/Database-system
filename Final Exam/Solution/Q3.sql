b9a414f4d6e962e40f7cec9fb83610b663f4ef7a
AhmedAli <89314691+MalikAhmedAli@users.noreply.github.com>
2022-06-08 23:23:19 +0500

    Add files via upload

---
+++ Final Exam/Solution/Q3.sql
@@ -0,0 +1,3 @@
+create procedure regions(IN region_name varchar(20)) BEGIN insert into regions values(country_name); END$$
+
+create procedure country(IN country_name varchar(20)) BEGIN insert into regions values(country_name); END$$
